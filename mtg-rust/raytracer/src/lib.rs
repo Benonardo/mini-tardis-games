d1872151c84860a66544bd859583e199c20ca821
Benonardo <75318636+Benonardo@users.noreply.github.com>
2024-02-12 20:51:42 +0100

    We are tracing some rays

---
+++ mtg-rust/raytracer/src/lib.rs
@@ -0,0 +1,62 @@
+use glam::{Vec2, Vec3, Vec4};
+use mtg_rust::{Canvas, ClickType, Game, Screen};
+
+mtg_rust::game_impl!(RayTracer);
+
+#[derive(Default)]
+pub struct RayTracer {
+    degrees: i32,
+}
+
+fn per_pixel(coord: Vec2, ray_origin: Vec3) -> Vec4 {
+    let ray_direction = Vec3::new(coord.x, coord.y, -1.0);
+    const RADIUS: f32 = 0.5;
+
+    let a = ray_direction.dot(ray_direction);
+    let b = 2.0 * ray_origin.dot(ray_direction);
+    let c = ray_origin.dot(ray_origin) - RADIUS * RADIUS;
+
+    let discriminant = b * b - 4.0 * a * c;
+
+    if discriminant < 0.0 {
+        return Vec4::new(0.0, 0.0, 0.0, 1.0);
+    }
+
+    let closest_t = (-b - discriminant.sqrt()) / (2.0 * a);
+
+    let hit_point = ray_direction * closest_t + ray_origin;
+    let normal = hit_point.normalize();
+
+    let light_dir = Vec3::new(-1.0, -1.0, -1.0).normalize();
+    let d = normal.dot(-light_dir).max(0.0);
+
+    let mut pixel_color = Vec4::new(1.0, 0.0, 1.0, 1.0) * d;
+    pixel_color.w = 1.0;
+    pixel_color
+}
+
+impl Game for RayTracer {
+    fn draw(&mut self, _screen: &Screen, canvas: &Canvas) {
+        let radians = (self.degrees as f32).to_radians();
+        let ray_origin = Vec3::new(radians.sin(), 0.0, radians.cos());
+        let width = canvas.get_width();
+        let height = canvas.get_height();
+        for x in 0..width {
+            for y in 0..height {
+                let coord = Vec2::new(
+                    x as f32 / width as f32 * 2.0 - 1.0,
+                    (width - y) as f32 / width as f32 * 2.0 - 1.0,
+                );
+                let pixel_color = per_pixel(coord, ray_origin);
+                let color = ((pixel_color.w * 255.0) as i32) << 24
+                    | ((pixel_color.x * 255.0) as i32) << 16
+                    | ((pixel_color.y * 255.0) as i32) << 8
+                    | ((pixel_color.z * 255.0) as i32);
+                canvas.set_pixel_argb(x, y, color);
+            }
+        }
+        self.degrees += 1;
+    }
+
+    fn on_click(&mut self, _screen: &Screen, _click_type: ClickType, _x: i32, _y: i32) {}
+}
--- mtg-rust/src/lib.rs
+++ mtg-rust/src/lib.rs
@@ -1,4 +1,5 @@
-#![cfg_attr(not(feature = "std"), no_std)]
+//! The rust crate for the Mini Tardis Games addon of the Minecraft mod Mini Tardis.  
+//! Setup your [`Game`] using [`game_impl`] and you're ready to go!
 
 mod ffi;
 
@@ -15,6 +16,7 @@ fn convert_str(str: &str) -> (i32, i32) {
 
 /// A level of importance for the [`log`] function.  
 /// Analogous to SLF4J's [`Level`](https://www.javadoc.io/doc/org.slf4j/slf4j-api/latest/org.slf4j/org/slf4j/event/Level.html) enum.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 #[repr(i32)]
 pub enum LogLevel {
     Error = 40,
@@ -41,6 +43,7 @@ pub fn nano_time() -> i64 {
 
 /// A category for a certain sound event, used by [`Screen::play_sound`].  
 /// Analogous to [the class with the same name in yarn mappings](https://maven.fabricmc.net/docs/yarn-1.20.1+build.10/net/minecraft/sound/SoundCategory.html).
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(i32)]
 pub enum SoundCategory {
     Master,
@@ -63,13 +66,13 @@ pub struct Screen {
 
 impl Screen {
     /// Returns a pseudorandom [`i32`] using the block entities' [`drawRandom`](https://github.com/enjarai/mini-tardis/blob/cd9041c0cd82eb7f92d4e48ea3c24d9a2ec62e24/src/main/java/dev/enjarai/minitardis/block/console/ScreenBlockEntity.java#L48) field.
-    pub fn random_i32() -> i32 {
+    pub fn random_i32(&self) -> i32 {
         unsafe { ffi::mtg_random_i32() }
     }
 
     /// Plays a sound at the screen's position with the specified `category`, `volume` and `pitch`.  
     /// The [`str`] `id` is in the format of an [`Identifier`](https://maven.fabricmc.net/docs/yarn-1.20.1+build.10/net/minecraft/util/Identifier.html) referring to a sound event.
-    pub fn play_sound(id: &str, category: SoundCategory, volume: f32, pitch: f32) {
+    pub fn play_sound(&self, id: &str, category: SoundCategory, volume: f32, pitch: f32) {
         let (id_address, id_len) = convert_str(id);
         unsafe {
             ffi::mtg_play_sound(id_address, id_len, category as i32, volume, pitch);
@@ -86,43 +89,43 @@ pub struct Canvas {
 impl Canvas {
     /// Get the width of the canvas, usually 128.  
     /// Analogous to the canvas' [`getWidth`](https://github.com/Patbox/map-canvas-api/blob/2dc8c9ab5ff2c5caa4cc29168b205224e95402ea/src/main/java/eu/pb4/mapcanvas/api/core/DrawableCanvas.java#L34) method.
-    pub fn get_width() -> i32 {
+    pub fn get_width(&self) -> i32 {
         unsafe { ffi::mtg_get_width() }
     }
 
     /// Get the height of the canvas, usually 96.  
     /// Analogous to the canvas' [`getHeight`](https://github.com/Patbox/map-canvas-api/blob/2dc8c9ab5ff2c5caa4cc29168b205224e95402ea/src/main/java/eu/pb4/mapcanvas/api/core/DrawableCanvas.java#L32) method.
-    pub fn get_height() -> i32 {
+    pub fn get_height(&self) -> i32 {
         unsafe { ffi::mtg_get_height() }
     }
 
     /// Get the raw color value at the certain `x` and `y` coordinates.  
     /// Analogous to the canvas' [`getRaw`](https://github.com/Patbox/map-canvas-api/blob/2dc8c9ab5ff2c5caa4cc29168b205224e95402ea/src/main/java/eu/pb4/mapcanvas/api/core/DrawableCanvas.java#L28) method.
-    pub fn get_raw_color(x: i32, y: i32) -> i32 {
+    pub fn get_raw_color(&self, x: i32, y: i32) -> i32 {
         unsafe { ffi::mtg_get_raw(x, y) }
     }
 
     /// Set the raw color value at the certain `x` and `y` coordinates.  
     /// Analogous to the canvas' [`setRaw`](https://github.com/Patbox/map-canvas-api/blob/2dc8c9ab5ff2c5caa4cc29168b205224e95402ea/src/main/java/eu/pb4/mapcanvas/api/core/DrawableCanvas.java#L30) method.
-    pub fn set_pixel_raw(x: i32, y: i32, color: i32) {
+    pub fn set_pixel_raw(&self, x: i32, y: i32, color: i32) {
         unsafe {
             ffi::mtg_set_raw(x, y, color);
         }
     }
 
     /// Set the RGB color value at the certain `x` and `y` coordinates.  
-    /// Analogous to calling the canvas' [`set`](https://github.com/Patbox/map-canvas-api/blob/2dc8c9ab5ff2c5caa4cc29168b205224e95402ea/src/main/java/eu/pb4/mapcanvas/api/core/DrawableCanvas.java#L20) method 
+    /// Analogous to calling the canvas' [`set`](https://github.com/Patbox/map-canvas-api/blob/2dc8c9ab5ff2c5caa4cc29168b205224e95402ea/src/main/java/eu/pb4/mapcanvas/api/core/DrawableCanvas.java#L20) method
     /// with the color argument returned by [`CanvasUtils.findClosestColor`](https://github.com/Patbox/map-canvas-api/blob/b9781dbdd439ff94ff58383ee8ada64928d4bf28/src/main/java/eu/pb4/mapcanvas/api/utils/CanvasUtils.java#L116).
-    pub fn set_pixel_rgb(x: i32, y: i32, color: i32) {
+    pub fn set_pixel_rgb(&self, x: i32, y: i32, color: i32) {
         unsafe {
             ffi::mtg_set_rgb(x, y, color);
         }
     }
 
     /// Set the ARGB color value at the certain `x` and `y` coordinates.  
-    /// Analogous to calling the canvas' [`set`](https://github.com/Patbox/map-canvas-api/blob/2dc8c9ab5ff2c5caa4cc29168b205224e95402ea/src/main/java/eu/pb4/mapcanvas/api/core/DrawableCanvas.java#L20) method 
+    /// Analogous to calling the canvas' [`set`](https://github.com/Patbox/map-canvas-api/blob/2dc8c9ab5ff2c5caa4cc29168b205224e95402ea/src/main/java/eu/pb4/mapcanvas/api/core/DrawableCanvas.java#L20) method
     /// with the color argument returned by [`CanvasUtils.findClosestColorARGB`](https://github.com/Patbox/map-canvas-api/blob/b9781dbdd439ff94ff58383ee8ada64928d4bf28/src/main/java/eu/pb4/mapcanvas/api/utils/CanvasUtils.java#L106).
-    pub fn set_pixel_argb(x: i32, y: i32, color: i32) {
+    pub fn set_pixel_argb(&self, x: i32, y: i32, color: i32) {
         unsafe {
             ffi::mtg_set_argb(x, y, color);
         }
@@ -131,7 +134,7 @@ impl Canvas {
     /// Draw a sprite at the certain `x` and `y` coordinates.  
     /// Analogous to calling the [`CanvasUtils.draw`](https://github.com/Patbox/map-canvas-api/blob/b9781dbdd439ff94ff58383ee8ada64928d4bf28/src/main/java/eu/pb4/mapcanvas/api/utils/CanvasUtils.java#L56) method
     /// with the source argument returned by [`TardisCanvasUtils.getSprite`](https://github.com/enjarai/mini-tardis/blob/cd9041c0cd82eb7f92d4e48ea3c24d9a2ec62e24/src/main/java/dev/enjarai/minitardis/canvas/TardisCanvasUtils.java#L41).
-    pub fn draw_inbuilt_sprite(x: i32, y: i32, name: &str) {
+    pub fn draw_inbuilt_sprite(&self, x: i32, y: i32, name: &str) {
         let (name_address, name_len) = convert_str(name);
         unsafe {
             ffi::mtg_draw_inbuilt_sprite(x, y, name_address, name_len);
@@ -140,10 +143,68 @@ impl Canvas {
 
     /// Draw text at the certain `x` and `y` coordinates.  
     /// Analogous to calling `DefaultFonts.VANILLA.drawText(canvas, text, x, y, size, CanvasUtils.findClosestColorARGB(argb));`.
-    pub fn draw_text(x: i32, y: i32, text: &str, size: i32, argb_color: i32) {
+    pub fn draw_text(&self, x: i32, y: i32, text: &str, size: i32, argb_color: i32) {
         let (text_address, text_len) = convert_str(text);
         unsafe {
             ffi::mtg_draw_text(x, y, text_address, text_len, size, argb_color);
         }
     }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum ClickType {
+    Left,
+    Right,
+}
+
+/// Connects a type implementing [`Game`] with the underlying WASM functions.  
+/// Use this macro at the top level once with your game type as the argument.
+#[macro_export]
+macro_rules! game_impl {
+    ($game_type:ident) => {
+        #[no_mangle]
+        pub extern "C" fn mtg_alloc_data() -> i32 {
+            $crate::_register_game::<$game_type>()
+        }
+
+        #[no_mangle]
+        pub extern "C" fn mtg_draw(data_ptr: i32) {
+            $crate::_draw::<$game_type>(data_ptr);
+        }
+
+        #[no_mangle]
+        pub extern "C" fn mtg_on_click(data_ptr: i32, r#type: i32, x: i32, y: i32) {
+            $crate::_on_click::<$game_type>(data_ptr, r#type, x, y);
+        }
+    };
+}
+
+pub trait Game: Default {
+    fn draw(&mut self, screen: &Screen, canvas: &Canvas);
+
+    fn on_click(&mut self, screen: &Screen, click_type: ClickType, x: i32, y: i32);
+}
+
+#[doc(hidden)]
+pub fn _register_game<G: Game>() -> i32 {
+    std::panic::set_hook(Box::new(|info| log(&info.to_string(), LogLevel::Error)));
+
+    Box::leak(Box::<G>::default()) as *mut _ as i32
+}
+
+#[doc(hidden)]
+pub fn _draw<G: Game>(data_ptr: i32) {
+    let game = unsafe { (data_ptr as *mut G).as_mut() }.expect("game was null in draw");
+    game.draw(&Screen { _dummy: () }, &Canvas { _dummy: () });
+}
+
+#[doc(hidden)]
+pub fn _on_click<G: Game>(data_ptr: i32, r#type: i32, x: i32, y: i32) {
+    let game = unsafe { (data_ptr as *mut G).as_mut() }.expect("game was null in on_click");
+    let click_type = match r#type {
+        0 => ClickType::Left,
+        1 => ClickType::Right,
+        other => panic!("unknown click type {other}"),
+    };
+    game.on_click(&Screen { _dummy: () }, click_type, x, y);
 }
