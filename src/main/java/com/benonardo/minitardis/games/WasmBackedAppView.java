9ed77c3ee03ce4972198c628c0c8ad8de0498615
Benonardo <75318636+Benonardo@users.noreply.github.com>
2024-02-12 11:57:08 +0100

    Add state and a bunch of other functions

--- src/main/java/com/benonardo/minitardis/games/WasmBackedAppView.java
+++ src/main/java/com/benonardo/minitardis/games/WasmBackedAppView.java
@@ -2,13 +2,20 @@ package com.benonardo.minitardis.games;
 
 import com.dylibso.chicory.runtime.*;
 import com.dylibso.chicory.runtime.Module;
+import com.dylibso.chicory.wasm.exceptions.ChicoryException;
 import com.dylibso.chicory.wasm.types.Value;
 import com.dylibso.chicory.wasm.types.ValueType;
-import dev.enjarai.minitardis.block.console.ConsoleScreenBlockEntity;
+import dev.enjarai.minitardis.block.console.ScreenBlockEntity;
+import dev.enjarai.minitardis.canvas.TardisCanvasUtils;
 import dev.enjarai.minitardis.component.screen.app.AppView;
 import eu.pb4.mapcanvas.api.core.DrawableCanvas;
+import eu.pb4.mapcanvas.api.font.DefaultFonts;
+import eu.pb4.mapcanvas.api.utils.CanvasUtils;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.ClickType;
+import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.event.Level;
 
@@ -18,16 +25,18 @@ public final class WasmBackedAppView implements AppView {
 
     private final ExportFunction draw;
     private final ExportFunction onClick;
+    @Nullable
+    private final ExportFunction drawBackground;
     private final int dataPtr;
     private final Instance instance;
     @Nullable
     private DrawableCanvas canvas = null;
     @Nullable
-    private ConsoleScreenBlockEntity blockEntity;
+    private ScreenBlockEntity blockEntity;
 
     public WasmBackedAppView(Module module) {
         var functions = this.new BuiltinFunctions();
-        instance = module.instantiate(new HostImports(functions.ALL));
+        instance = module.instantiate(new HostImports(functions.all));
         this.draw = instance.export("mtg_draw");
         this.onClick = instance.export("mtg_on_click");
         var allocResult = instance.export("mtg_alloc_data").apply();
@@ -35,39 +44,71 @@ public final class WasmBackedAppView implements AppView {
             throw new RuntimeException("alloc_data() returned multiple values");
         }
         this.dataPtr = allocResult[0].asInt();
+
+        var drawBackground = (ExportFunction)null;
+        try {
+            drawBackground = instance.export("mtg_draw_background");
+        } catch (ChicoryException ignored) {}
+        this.drawBackground = drawBackground;
     }
 
     @Override
-    public void draw(ConsoleScreenBlockEntity blockEntity, DrawableCanvas canvas) {
+    public void draw(ScreenBlockEntity blockEntity, DrawableCanvas canvas) {
         this.canvas = canvas;
         this.blockEntity = blockEntity;
         try {
-            draw.apply(Value.i32(dataPtr));
+            synchronized (this) {
+                draw.apply(Value.i32(dataPtr));
+            }
         } catch (Exception e) {
             MiniTardisGames.LOGGER.error("WASM draw", e);
+            throw new AssertionError();
         }
         this.canvas = null;
         this.blockEntity = null;
     }
 
     @Override
-    public boolean onClick(ConsoleScreenBlockEntity blockEntity, ServerPlayerEntity player, ClickType type, int x, int y) {
+    public boolean onClick(ScreenBlockEntity blockEntity, ServerPlayerEntity player, ClickType type, int x, int y) {
         this.blockEntity = blockEntity;
         try {
-            onClick.apply(Value.i32(dataPtr), Value.i32(type.ordinal()), Value.i32(x), Value.i32(y));
+            synchronized (this) {
+                onClick.apply(Value.i32(dataPtr), Value.i32(type.ordinal()), Value.i32(x), Value.i32(y));
+            }
         } catch (Exception e) {
             MiniTardisGames.LOGGER.error("WASM on_click", e);
+            throw new AssertionError();
         }
         this.blockEntity = null;
         return false;
+    }
+
+    @Override
+    public void drawBackground(ScreenBlockEntity blockEntity, DrawableCanvas canvas) {
+        if (this.drawBackground == null) {
+            AppView.super.drawBackground(blockEntity, canvas);
+        } else {
+            this.canvas = canvas;
+            this.blockEntity = blockEntity;
+            try {
+                synchronized (this) {
+                    drawBackground.apply(Value.i32(dataPtr));
+                }
+            } catch (Exception e) {
+                MiniTardisGames.LOGGER.error("WASM draw_background", e);
+                throw new AssertionError();
+            }
+            this.canvas = null;
+            this.blockEntity = null;
+        }
     }
 
     private final class BuiltinFunctions {
 
         public final HostFunction log = new HostFunction(
                 (instance, args) -> {
-                    var len = args[0].asInt();
-                    var offset = args[1].asInt();
+                    var offset = args[0].asInt();
+                    var len = args[1].asInt();
                     var message = instance.memory().readString(offset, len);
                     var level = args[2].asInt();
                     MiniTardisGames.LOGGER.atLevel(Level.intToLevel(level)).log(message);
@@ -87,7 +128,7 @@ public final class WasmBackedAppView implements AppView {
                 },
                 "mini_tardis_games",
                 "mtg_random_i32",
-                List.of(ValueType.I32, ValueType.I32, ValueType.I32),
+                List.of(),
                 List.of()
         );
         public final HostFunction getWidth = new HostFunction(
@@ -147,7 +188,103 @@ public final class WasmBackedAppView implements AppView {
                 List.of(ValueType.I32, ValueType.I32, ValueType.I32),
                 List.of()
         );
-        public final HostFunction[] ALL = {log, randomI32, getWidth, getHeight, getRaw, setRaw};
+        public final HostFunction setRgb = new HostFunction(
+                (instance, args) -> {
+                    if (canvas == null) {
+                        throw new IllegalStateException("Called set_rgb(x, y, color) while not currently drawing");
+                    }
+                    var x = args[0].asInt();
+                    var y = args[1].asInt();
+                    var color = args[2].asInt();
+                    canvas.set(x, y, CanvasUtils.findClosestColor(color));
+                    return Value.EMPTY_VALUES;
+                },
+                "mini_tardis_games",
+                "mtg_set_rgb",
+                List.of(ValueType.I32, ValueType.I32, ValueType.I32),
+                List.of()
+        );
+        public final HostFunction setArgb = new HostFunction(
+                (instance, args) -> {
+                    if (canvas == null) {
+                        throw new IllegalStateException("Called set_argb(x, y, color) while not currently drawing");
+                    }
+                    var x = args[0].asInt();
+                    var y = args[1].asInt();
+                    var color = args[2].asInt();
+                    canvas.set(x, y, CanvasUtils.findClosestColorARGB(color));
+                    return Value.EMPTY_VALUES;
+                },
+                "mini_tardis_games",
+                "mtg_set_argb",
+                List.of(ValueType.I32, ValueType.I32, ValueType.I32),
+                List.of()
+        );
+        public final HostFunction drawInbuiltSprite = new HostFunction(
+                (instance, args) -> {
+                    if (canvas == null) {
+                        throw new IllegalStateException("Called draw_inbuilt_sprite(x, y, name_len, name_offset) while not currently drawing");
+                    }
+                    var x = args[0].asInt();
+                    var y = args[1].asInt();
+                    var name_offset = args[2].asInt();
+                    var name_len = args[3].asInt();
+                    var name = instance.memory().readString(name_offset, name_len);
+                    CanvasUtils.draw(canvas, x, y, TardisCanvasUtils.getSprite(name));
+                    return Value.EMPTY_VALUES;
+                },
+                "mini_tardis_games",
+                "mtg_draw_inbuilt_sprite",
+                List.of(ValueType.I32, ValueType.I32, ValueType.I32, ValueType.I32),
+                List.of()
+        );
+        public final HostFunction drawText = new HostFunction(
+                (instance, args) -> {
+                    if (canvas == null) {
+                        throw new IllegalStateException("Called draw_text(text_offset, tex_len, x, y, argb) while not currently drawing");
+                    }
+                    var textOffset = args[0].asInt();
+                    var textLen = args[1].asInt();
+                    var text = instance.memory().readString(textOffset, textLen);
+                    var x = args[2].asInt();
+                    var y = args[3].asInt();
+                    var size = args[4].asInt();
+                    var argb = args[5].asInt();
+                    DefaultFonts.VANILLA.drawText(canvas, text, x, y, size, CanvasUtils.findClosestColorARGB(argb));
+                    return Value.EMPTY_VALUES;
+                },
+                "mini_tardis_games",
+                "mtg_draw_text",
+                List.of(ValueType.I32, ValueType.I32, ValueType.I32, ValueType.I32, ValueType.I32, ValueType.I32),
+                List.of()
+        );
+        public final HostFunction playSound = new HostFunction(
+                (instance, args) -> {
+                    if (blockEntity == null) {
+                        throw new IllegalStateException("Called play_sound() while not currently in a context???");
+                    }
+                    var id_offset = args[0].asInt();
+                    var id_len = args[1].asInt();
+                    var id = instance.memory().readString(id_offset, id_len);
+                    var category = SoundCategory.values()[args[2].asInt()];
+                    var volume = args[3].asFloat();
+                    var pitch = args[4].asFloat();
+                    blockEntity.getWorld().playSound(null, blockEntity.getPos(), SoundEvent.of(new Identifier(id)), category, volume, pitch);
+                    return Value.EMPTY_VALUES;
+                },
+                "mini_tardis_games",
+                "mtg_play_sound",
+                List.of(ValueType.I32, ValueType.I32, ValueType.I32, ValueType.F32, ValueType.F32),
+                List.of()
+        );
+        public final HostFunction nanoTime = new HostFunction(
+                (instance, args) -> new Value[]{Value.i64(System.nanoTime())},
+                "mini_tardis_games",
+                "mtg_nano_time",
+                List.of(),
+                List.of(ValueType.I64)
+        );
+        public final HostFunction[] all = {log, randomI32, getWidth, getHeight, getRaw, setRaw, setRgb, setArgb, drawInbuiltSprite, drawText, playSound, nanoTime};
 
         private BuiltinFunctions() {
         }
--- testgame/testgame.c
+++ testgame/testgame.c
@@ -1,53 +1,36 @@
 #include <stdint.h>
-#include <string.h>
-#include <stdio.h>
-#include <emscripten.h>
 
-typedef enum click_type
-{
-    LEFT,
-    RIGHT
-} click_type;
+typedef enum click_type { LEFT, RIGHT } click_type;
 
 #ifdef __cplusplus
-extern "C"
+extern "C" {
 #endif
 
-    void
-    mtg_log(int32_t len, int32_t offset, int32_t level) __attribute__((__import_module__("mini_tardis_games")));
-int32_t mtg_get_width(void) __attribute__((__import_module__("mini_tardis_games")));
-int32_t mtg_get_height(void) __attribute__((__import_module__("mini_tardis_games")));
-void mtg_set_raw(int32_t x, int32_t y, int32_t color) __attribute__((__import_module__("mini_tardis_games")));
-int32_t mtg_random_i32(void) __attribute__((__import_module__("mini_tardis_games")));
+__attribute__((import_module("mini_tardis_games"))) void
+mtg_log(int32_t len, int32_t offset, int32_t level);
+__attribute__((import_module("mini_tardis_games"))) int32_t mtg_get_width(void);
+__attribute__((import_module("mini_tardis_games"))) int32_t
+mtg_get_height(void);
+__attribute__((import_module("mini_tardis_games"))) void
+mtg_set_raw(int32_t x, int32_t y, int32_t color);
+__attribute__((import_module("mini_tardis_games"))) int32_t
+mtg_random_i32(void);
 
-EMSCRIPTEN_KEEPALIVE
-void mtg_draw(int32_t data_ptr)
-{
-    int width = mtg_get_width();
-    int height = mtg_get_height();
-    for (int32_t x = 0; x < width; x++)
-    {
-        for (int32_t y = 0; y < height; y++)
-        {
-            mtg_set_raw(x, y, mtg_random_i32() & 127);
-        }
+void mtg_draw(int32_t data_ptr) {
+  int32_t width = mtg_get_width();
+  int32_t height = mtg_get_height();
+  for (int32_t x = 0; x < width; x++) {
+    for (int32_t y = 0; y < height; y++) {
+      mtg_set_raw(x, y, mtg_random_i32() & 127);
     }
+  }
 }
 
-EMSCRIPTEN_KEEPALIVE
-void mtg_on_click(int32_t data_ptr, click_type type, int32_t x, int32_t y)
-{
-    const char *type_str = type == LEFT ? "LEFT" : "RIGHT";
-    char log_str[32];
-    sprintf(log_str, "%s at %d,%d", type_str, x, y);
-    mtg_log(strlen(log_str), (int32_t)log_str, 20);
+void mtg_on_click(int32_t data_ptr, click_type type, int32_t x, int32_t y) {
+  mtg_log(type == LEFT ? 4 : 5, (int32_t)(type == LEFT ? "LEFT" : "RIGHT"), 20);
 }
 
-EMSCRIPTEN_KEEPALIVE
-int32_t mtg_alloc_data(void)
-{
-    return -1;
-}
+int32_t mtg_alloc_data(void) { return -1; }
 
 #ifdef __cplusplus
 }
